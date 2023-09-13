f5462444b4af3e35d9fe6e48171e3e09303d84a3
rahat97 <iamrahat13@gmail.com>
2023-09-13 11:19:11 +0600

    zip functin

---
+++ zip_function.py
@@ -0,0 +1,6 @@
+sId = [23001, 23002, 23003, 23004, 23005, 23006, 23007]
+sName = ["Rahim", "Karim", "Moni", "Rani", "Joshi", "Rasel", "Firoz"]
+
+print(list(zip(sId, sName)))
+
+print(list(zip(sId, sName, "ACDBADB")))
