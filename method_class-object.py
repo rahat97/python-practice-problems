f48a20134c8d6f9a175e9f1ea8c914f7ad6142aa
rahat97 <iamrahat13@gmail.com>
2023-09-14 20:08:50 +0600

    methods in class, object

--- method_class-object.py
+++ method_class-object.py
@@ -1,18 +1,24 @@
-class student:
+class Student:
     roll = ""
     gpa = ""
+
+    def set_value(self, roll, gpa):
+        self.roll = roll
+        self.gpa = gpa
 
     def display(self):
         print(f"Roll : {self.roll}, GPA : {self.gpa}")
 
 
-rahim = student()
-rahim.roll = 101
-rahim.gpa = 3.7
+rahim = Student()
+'''rahim.roll = 101
+rahim.gpa = 3.65'''
+rahim.set_value(101, 3.65)
 rahim.display()
 
 
-karim = student()
-karim.roll = 102
-karim.gpa = 2.9
+karim = Student()
+'''karim.roll = 102
+karim.gpa = 3.11'''
+karim.set_value(102, 3.11)
 karim.display()
