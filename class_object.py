67acc86ed87efd2f45f710c561317446b0c147db
rahat97 <iamrahat13@gmail.com>
2023-09-14 19:56:46 +0600

    class and object

--- class_object.py
+++ class_object.py
@@ -9,3 +9,10 @@ print(isinstance(rahim, student))
 rahim.roll = 101
 rahim.gpa = 3.7
 print(f"Roll : {rahim.roll}, GPA : {rahim.gpa}")
+
+karim = student()
+print(isinstance(karim, student))
+
+karim.roll = 102
+karim.gpa = 2.9
+print(f"Roll : {karim.roll}, GPA : {karim.gpa}")
