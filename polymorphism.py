7d75d3b5a17431b321ab859e1e1220a75db56fa2
rahat97 <iamrahat13@gmail.com>
2023-09-15 10:40:41 +0600

    polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,12 @@
+# built in polymorphic function
+print(len("Rakib Rahat"))
+print(len([11, 17, 23]))
+
+
+# user defined polymorphic function
+def add(x, y, z=0):
+    return x + y + z
+
+
+print(add(3, 5))
+print(add(5, 7, 11))
