f26d15fa5b8b205c777dc73f931a691f4589fb77
rahat97 <iamrahat13@gmail.com>
2023-09-15 11:11:53 +0600

    creating own module

---
+++ Module/area.py
@@ -0,0 +1,7 @@
+def triangle_area(b, h):
+    print(f"Area of Triangle is : {0.5 * b * h}")
+
+
+def rectangle_area(b, h):
+    print(f"Area of Triangle is : {b * h}")
+    
---
+++ Module/myModule_01.py
@@ -0,0 +1,4 @@
+from area import rectangle_area, triangle_area # * -> for all methods
+
+rectangle_area(10, 20)
+triangle_area(12, 25)
